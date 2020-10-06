f1b76997afddd3a06f846d4d7b2a89039df10a70
Cindy Okino <cindy.okino@gmail.com>
2020-10-06 14:27:25 -0400

    Commercial JAVA: Modify summary section

--- Commercial_Controller.java
+++ Commercial_Controller.java
@@ -8,26 +8,25 @@
  0- BATTERY CLASS
     0a- Constructor and its attributes
     0b- Method toString
-    0c- Methods to create a list: createColumnsList
-    0d- Methods for logic: calculateNumberOfFloorsPerColumn
+    0c- Methods to create a list: createColumnsList, createListsInsideColumns
+    0d- Methods for logic: calculateNumberOfFloorsPerColumn, setColumnValues
  1- COLUMN CLASS
     1a- Constructor and its attributes
     1b- Method toString
     1c- Methods to create a list: createElevatorsList, createButtonsUpList, createButtonsDownList
-    1d- Methods for logic: findElevator, findNearestElevator
+    1d- Methods for logic: optimizeDisplacement, findElevator, findNearestElevator
     1e- Entry method: requestElevator
  2- ELEVATOR CLASS
     2a- Constructor and its attributes
     2b- Method toString
     2c- Methods to create a list: createFloorDoorsList, createDisplaysList, createFloorButtonsList, addFloorToFloorList
-    2d- Methods for logic: moveElevator, moveUp, moveDown, updateDisplays, openDoors, closeDoors, checkWeight, checkObstruction, deleteFloorFromList
+    2d- Methods for logic: moveElevator, moveUp, moveDown, updateDisplays, openDoors, closeDoors, checkWeight, checkObstruction, addFloorToFloorList, deleteFloorFromList
     2e- Entry method: requestFloor
  3- DOOR CLASS
  4- BUTTON CLASS
  5- DISPLAY CLASS
  6- ENUMS
  7- TESTING PROGRAM - SCENARIOS
- 8- TEST YOUR SCENARIO
  9- TESTING PROGRAM - CALL SCENARIOS
 
  CONTROLLED OBJECTS:
@@ -45,7 +44,6 @@
   ** ************************************************** */
 
 
-import javax.swing.text.html.Option;
 import java.time.LocalTime;
 import java.util.*;
 import java.util.stream.Collectors;
