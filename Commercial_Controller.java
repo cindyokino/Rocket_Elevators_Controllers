1de6e74c6fdcce622eae1dfd41a310a3b94b266c
Cindy Okino <cindy.okino@gmail.com>
2020-10-07 16:14:31 -0400

    Commercial JAVA: Fix numberOfBasements bug

--- Commercial_Controller.java
+++ Commercial_Controller.java
@@ -34,7 +34,7 @@
  Columns: controls a list of N elevators
  Elevators: controls doors, buttons, displays
 
- numberOfBasements                                                  //Use a negative number
+ numberOfBasements                                                  
  numberOfFloors                                                     //Floors of the building excluding the number of basements
  totalNumberOfFloors = numberOfFloors + Math.abs(numberOfBasements) //Transform the number of basements to a positive number
  minBuildingFloor                                                   //Is equal to 1 OR equal the numberOfBasements if there is a basement
@@ -57,7 +57,7 @@ class Battery {
     int minBuildingFloor;                  //Is equal to 1 OR equal the numberOfBasements if there is a basement
     int maxBuildingFloor;                  //Is the last floor of the building
     int numberOfFloors;                    //Floors of the building excluding the number of basements
-    int numberOfBasements;                 //Is a negative number
+    int numberOfBasements;                 
     int totalNumberOfFloors;               //numberOfFloors + Math.abs(numberOfBasements)
     int numberOfElevatorsPerColumn;
     int numberOfFloorsPerColumn;
@@ -103,7 +103,7 @@ class Battery {
         columnsList.forEach(column -> {
             column.createElevatorsList();
             column.createButtonsUpList();
-            column.createButtonsDownList(numberOfBasements);
+            column.createButtonsDownList();
         });
     }
 
@@ -111,7 +111,7 @@ class Battery {
     //----------------- Methods for logic -----------------//
     /* ******* LOGIC TO FIND THE FLOORS SERVED PER EACH COLUMN ******* */
     public int calculateNumberOfFloorsPerColumn() {
-        numberOfFloors = totalNumberOfFloors + numberOfBasements; //numberOfBasements is negative
+        numberOfFloors = totalNumberOfFloors - numberOfBasements; 
         int numberOfFloorsPerColumn;
 
         if (this.numberOfBasements > 0) { //if there is basement floors
@@ -155,7 +155,7 @@ class Battery {
     /* ******* LOGIC TO SET THE minFloor AND maxFloor FOR THE BASEMENT COLUMN ******* */
     private void initializeBasementColumnFloors() {
         this.columnsList.get(0).numberServedFloors = (this.numberOfBasements + 1); //+1 is the RDC
-        this.columnsList.get(0).minFloor = numberOfBasements; //the minFloor of basement is a negative number
+        this.columnsList.get(0).minFloor = numberOfBasements * -1; //the minFloor of basement is a negative number
         this.columnsList.get(0).maxFloor = 1; //1 is the RDC
     }
 
@@ -212,7 +212,7 @@ class Column {
         this.status = columnStatus;
         this.numberOfElevatorsPerColumn = numberOfElevatorsPerColumn;
         this.numberServedFloors = numberServedFloors;
-        this.numberOfBasements = numberOfBasements;
+        this.numberOfBasements = numberOfBasements * -1;
         this.battery = battery;
         this.elevatorsList = new ArrayList<>();
         this.buttonsUpList = new ArrayList<>();
@@ -245,11 +245,11 @@ class Column {
     }
 
     /* ******* CREATE A LIST WITH DOWN BUTTONS FROM THE SECOND FLOOR TO THE LAST FLOOR ******* */
-    public void createButtonsDownList(int numberOfBasements) {
+    public void createButtonsDownList() {
         buttonsDownList.add(new Button(1, ButtonStatus.OFF, 1));
         int minBuildingFloor;
         if (numberOfBasements > 0) {
-            minBuildingFloor = numberOfBasements;
+            minBuildingFloor = numberOfBasements * -1;
         } else {
             minBuildingFloor = 1;
         }
@@ -750,9 +750,10 @@ public class Commercial_Controller {
 
     /* ******* CREATE SCENARIO 1 ******* */
     public static void scenario1() {
-        System.out.println("****************************** SCENARIO 1: ******************************");
+        System.out.println("\n****************************** SCENARIO 1: ***************************\n");
         Battery batteryScenario1 = new Battery(1, 4, 66, 6, 5, BatteryStatus.ACTIVE);
         System.out.println(batteryScenario1);
+        System.out.println();
         batteryScenario1.columnsList.forEach(System.out::println); //batteryScenario1.columnsList.forEach(column -> System.out.println(column));
         System.out.println();
         //--------- ElevatorB1 ---------
@@ -788,15 +789,15 @@ public class Commercial_Controller {
         batteryScenario1.columnsList.get(1).requestElevator(1, Direction.UP); //parameters (requestedFloor, buttonDirection.UP/DOWN)
         batteryScenario1.columnsList.get(1).elevatorsList.get(4).requestFloor(20); //parameters (requestedFloor)
         System.out.println("=========================================================================");
-        System.out.println();
     }
 
     /* ******* CREATE SCENARIO 2 ******* */
     public static void scenario2() {
-        System.out.println("****************************** SCENARIO 2: ******************************");
+        System.out.println("\n****************************** SCENARIO 2: ******************************\n");
         Battery batteryScenario2 = new Battery(1, 4, 66, 6, 5, BatteryStatus.ACTIVE);
         System.out.println(batteryScenario2);
-        batteryScenario2.columnsList.forEach(System.out::println); //batteryScenario2.columnsList.forEach(column -> System.out.println(column));
+        System.out.println();
+        batteryScenario2.columnsList.forEach(System.out::println);
         System.out.println();
         //--------- ElevatorC1 ---------;
         batteryScenario2.columnsList.get(2).elevatorsList.get(0).floor = 1;
@@ -830,17 +831,16 @@ public class Commercial_Controller {
         System.out.println(">> User request to go to floor <36>");
         batteryScenario2.columnsList.get(2).requestElevator(1, Direction.UP); //parameters (requestedFloor, buttonDirection.UP/DOWN)
         batteryScenario2.columnsList.get(2).elevatorsList.get(0).requestFloor(36); //parameters (requestedFloor)
-//        batteryScenario2.columnsList.get(2).elevatorsList.get(0).moveElevator(36);
         System.out.println("=========================================================================");
-        System.out.println();
     }
 
     /* ******* CREATE SCENARIO 3 ******* */
     public static void scenario3() {
-        System.out.println("****************************** SCENARIO 3: ******************************");
-        Battery batteryScenario3 = new Battery(1, 4, 66, -6, 5, BatteryStatus.ACTIVE);
+        System.out.println("\n****************************** SCENARIO 3: ******************************\n");
+        Battery batteryScenario3 = new Battery(1, 4, 66, 6, 5, BatteryStatus.ACTIVE);
         System.out.println(batteryScenario3);
-        batteryScenario3.columnsList.forEach(System.out::println); //batteryScenario3.columnsList.forEach(column -> System.out.println(column));
+        System.out.println();
+        batteryScenario3.columnsList.forEach(System.out::println);
         System.out.println();
         batteryScenario3.columnsList.get(3).elevatorsList.get(0).floor = 58;
         batteryScenario3.columnsList.get(3).elevatorsList.get(0).status = ElevatorStatus.DOWN;
@@ -870,18 +870,18 @@ public class Commercial_Controller {
         batteryScenario3.columnsList.get(3).requestElevator(54, Direction.DOWN); //parameters (requestedFloor, buttonDirection.UP/DOWN)
         batteryScenario3.columnsList.get(3).elevatorsList.get(0).requestFloor(1); //parameters (requestedFloor)
         System.out.println("=========================================================================");
-        System.out.println();
     }
 
     /* ******* CREATE SCENARIO 4 ******* */
-    public static void scenario4() { //use of negative number to indicate SS / basement
-        System.out.println("****************************** SCENARIO 4: ******************************");
+    public static void scenario4() { 
+        System.out.println("\n****************************** SCENARIO 4: ******************************\n");
         Battery batteryScenario4 = new Battery(1, 4, 66, 6, 5, BatteryStatus.ACTIVE);
         System.out.println(batteryScenario4);
-        batteryScenario4.columnsList.forEach(System.out::println); //batteryScenario4.columnsList.forEach(column -> System.out.println(column));
+        System.out.println();
+        batteryScenario4.columnsList.forEach(System.out::println);
         System.out.println();
         //--------- ElevatorA1 ---------
-        batteryScenario4.columnsList.get(0).elevatorsList.get(0).floor = -4;
+        batteryScenario4.columnsList.get(0).elevatorsList.get(0).floor = -4; //use of negative numbers to indicate SS / basement
         batteryScenario4.columnsList.get(0).elevatorsList.get(0).status = ElevatorStatus.IDLE;
 
         //--------- ElevatorA2 ---------
@@ -889,21 +889,20 @@ public class Commercial_Controller {
         batteryScenario4.columnsList.get(0).elevatorsList.get(1).status = ElevatorStatus.IDLE;
 
         //--------- ElevatorA3 ---------
-        batteryScenario4.columnsList.get(0).elevatorsList.get(2).floor = -3;
+        batteryScenario4.columnsList.get(0).elevatorsList.get(2).floor = -3; //use of negative numbers to indicate SS / basement
         batteryScenario4.columnsList.get(0).elevatorsList.get(2).status = ElevatorStatus.DOWN;
         batteryScenario4.columnsList.get(0).elevatorsList.get(2).addFloorToFloorList(-5);
 
         //--------- ElevatorA4 ---------
-        batteryScenario4.columnsList.get(0).elevatorsList.get(3).floor = -6;
+        batteryScenario4.columnsList.get(0).elevatorsList.get(3).floor = -6; //use of negative numbers to indicate SS / basement
         batteryScenario4.columnsList.get(0).elevatorsList.get(3).status = ElevatorStatus.UP;
         batteryScenario4.columnsList.get(0).elevatorsList.get(3).addFloorToFloorList(1);
 
         //--------- ElevatorA5 ---------
-        batteryScenario4.columnsList.get(0).elevatorsList.get(4).floor = -1;
+        batteryScenario4.columnsList.get(0).elevatorsList.get(4).floor = -1; //use of negative numbers to indicate SS / basement
         batteryScenario4.columnsList.get(0).elevatorsList.get(4).status = ElevatorStatus.DOWN;
         batteryScenario4.columnsList.get(0).elevatorsList.get(4).addFloorToFloorList(-6);
 
-        System.out.println();
         batteryScenario4.columnsList.get(0).elevatorsList.forEach(System.out::println);
         System.out.println();
         System.out.println("Person 1: (elevator A4 is expected)"); //elevator expected
